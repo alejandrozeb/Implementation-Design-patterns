33c663b96ddf21d8a222d1ee94d6cc98aef457c9
Alejandro Zeballos <alejandro.zeballos@fundacion-jala.org>
2022-10-04 22:33:57 -0400

    add strategy pattern on js

---
+++ strategyJs/strategy.js
@@ -0,0 +1,60 @@
+var Shipping = function () {
+    this.company = "";
+};
+
+Shipping.prototype = {
+    setStrategy: function (company) {
+        this.company = company;
+    },
+
+    calculate: function (package) {
+        return this.company.calculate(package);
+    }
+};
+
+var UPS = function () {
+    this.calculate = function (package) {
+        // calculations..
+        // return searched list 1 by package criteria
+        return "$45.95";
+    }
+};
+
+var USPS = function () {
+    this.calculate = function (package) {
+        // calculations...
+        // return searched list 2 by package criteria
+        return "$39.40";
+    }
+};
+
+var Fedex = function () {
+    this.calculate = function (package) {
+        // calculations...
+        // return searched list 3 by package criteria
+        return "$43.20";
+    }
+};
+
+function run() {
+
+    var package = { from: "76712", to: "10012", weigth: "lkg" };
+
+    // the 3 strategies
+
+    var ups = new UPS();
+    var usps = new USPS();
+    var fedex = new Fedex();
+
+    var shipping = new Shipping();
+
+    shipping.setStrategy(ups);
+    console.log("UPS Strategy: " + shipping.calculate(package));
+    shipping.setStrategy(usps);
+    console.log("USPS Strategy: " + shipping.calculate(package));
+    shipping.setStrategy(fedex);
+    console.log("Fedex Strategy: " + shipping.calculate(package));
+}
+
+
+run();
